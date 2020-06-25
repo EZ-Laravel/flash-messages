ade41f7dd311c1d14f689f2ece4e520bc7151507
Nick Verheijen <verheijen.webdevelopment@gmail.com>
2020-06-25 07:11:59 +0200

    Cleaned & updated the service provider

--- src/FlashServiceProvider.php
+++ src/FlashServiceProvider.php
@@ -7,31 +7,19 @@ use Illuminate\Support\ServiceProvider;
 
 class FlashServiceProvider extends ServiceProvider
 {
-    public function boot()
-    {
-        // Setup integration & publishing of the config file
-        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'flash');
-        $this->publishes([
-            __DIR__.'/../config/config.php' => config_path('flash.php'),
-        ], 'config');
-
-        // Setup view loading & publishing
-        $this->loadViewsFrom(__DIR__.'/../resources/views', 'flash');
-        $this->publishes([
-            __DIR__.'/../resources/views' => base_path('resources/views/vendor/flash'),
-        ], 'views');
-
-        // Setup component publishing
-        $this->publishes([
-            __DIR__.'/../resources/js/components' => base_path('resources/js/components'),
-        ], 'vue');
-
-        // Compose views
-        $this->composeViews();
-    }
-
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
     public function register()
     {
+        // Load the config file
+        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'flash');
+
+        // Load the views
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'flash');
+
         // Bind the LaravelSessionStorage implementation to the SessionStore interface
         $this->app->bind('EZ\FlashMessages\SessionStore', 'EZ\FlashMessages\LaravelSessionStore');
 
@@ -39,6 +27,26 @@ class FlashServiceProvider extends ServiceProvider
         $this->app->singleton('flash', function ($app) {
             return $this->app->make('EZ\FlashMessages\FlashService');
         });
+    }
+    
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // Setup publishing of the config file
+        $this->publishes([__DIR__.'/../config/config.php' => config_path('flash.php')], 'config');
+
+        // Setup publishing of the views
+        $this->publishes([__DIR__.'/../resources/views' => base_path('resources/views/vendor/flash')], 'views');
+
+        // Setup component publishing
+        $this->publishes([__DIR__.'/../resources/js/components' => base_path('resources/js/components')], 'vue');
+
+        // Compose views
+        $this->composeViews();
     }
 
     private function composeViews()
